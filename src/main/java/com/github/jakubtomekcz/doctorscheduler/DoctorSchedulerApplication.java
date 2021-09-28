245dee5b1812015220e597c5154eb47e532e0fb7
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-09-28 21:28:35 +0200

    project created

---
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/DoctorSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jakubtomekcz.doctorscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorSchedulerApplication.class, args);
+    }
+
+}
