40facb650673b6b7803b64e421de6d4b3e18b504
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-10-04 17:01:41 +0200

    added postMapping

--- src/main/java/com/github/jakubtomekcz/doctorscheduler/controller/MainController.java
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/controller/MainController.java
@@ -2,6 +2,10 @@ package com.github.jakubtomekcz.doctorscheduler.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -9,6 +13,12 @@ public class MainController {
     @GetMapping("/")
     public String get() {
         return "index";
+    }
+
+    @PostMapping("/")
+    public ModelAndView uploadPreferenceTable(@RequestParam("fileToUpload") MultipartFile fileToUpload) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 
 }
