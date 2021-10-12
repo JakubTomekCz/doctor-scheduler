11787281507564e5bdf0568cb417d8f8abb9505e
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-10-12 10:32:49 +0200

    enhanced PreferenceTable

--- src/main/java/com/github/jakubtomekcz/doctorscheduler/model/PreferenceTable.java
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/model/PreferenceTable.java
@@ -1,4 +1,17 @@
 package com.github.jakubtomekcz.doctorscheduler.model;
 
+
+import com.github.jakubtomekcz.doctorscheduler.constant.PreferenceType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@EqualsAndHashCode
 public class PreferenceTable {
+
+    private final Map<String, Map<String, PreferenceType>> data = new HashMap<>();
+
 }
