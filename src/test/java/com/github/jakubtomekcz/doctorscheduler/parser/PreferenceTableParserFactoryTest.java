e2cc36e07f8a3540fe5455cc72e56b8043279f77
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-10-14 22:17:07 +0200

    checkstyle

--- src/test/java/com/github/jakubtomekcz/doctorscheduler/parser/PreferenceTableParserFactoryTest.java
+++ src/test/java/com/github/jakubtomekcz/doctorscheduler/parser/PreferenceTableParserFactoryTest.java
@@ -4,7 +4,6 @@ package com.github.jakubtomekcz.doctorscheduler.parser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
