2ae7d88a008e874b62a73cfa4205e22a002bc003
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-10-16 22:51:37 +0200

    scheduler criteria suggested

--- src/main/java/com/github/jakubtomekcz/doctorscheduler/scheduler/SimpleHeuristicScheduler.java
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/scheduler/SimpleHeuristicScheduler.java
@@ -3,6 +3,26 @@ package com.github.jakubtomekcz.doctorscheduler.scheduler;
 import com.github.jakubtomekcz.doctorscheduler.model.PreferenceTable;
 import com.github.jakubtomekcz.doctorscheduler.model.Schedule;
 
+/**
+ * Creates a schedule based on given preferences.
+ * Attempts to maximize satisfaction.
+ * <p>
+ * Satisfaction criteria:
+ * <p>
+ * Priority 1 - must satisfy or fail
+ * Preference = NO => must not select person
+ * One person must not be selected for two consecutive days or second day after the first service (needs 2 days rest)
+ * <p>
+ * Priority 2
+ * Maximize total number of preferred days satisfied giving priority to less satisfied persons
+ * (maximize number of 1x satisfied, then 2x satisfied etc.)
+ * <p>
+ * Priority 3
+ * Minimize the maximum number of services per person (nobody is overloaded)
+ * <p>
+ * Priority 4
+ * Maximize the minimal number of services per person (nobody is slacking off)
+ */
 public class SimpleHeuristicScheduler implements Scheduler {
 
     @Override
