1a85e701c0217c7b4f2a2a3d32ae4a830b538b40
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-11-10 21:36:47 +0100

    Date formatting added

--- src/main/java/com/github/jakubtomekcz/doctorscheduler/model/Date.java
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/model/Date.java
@@ -1,13 +1,40 @@
 package com.github.jakubtomekcz.doctorscheduler.model;
 
-import java.text.SimpleDateFormat;
+import lombok.extern.slf4j.Slf4j;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+@Slf4j
 public record Date(java.util.Date value) {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd MMM yyyy");
+    private static final String ENGLISH_FORMAT = "EEE dd MMM yyyy";
+    private static final String ENGLISH_DAY_IN_WEEK_FORMAT = "EEE";
+    private static final String CZECH_DAY_FORMAT = "d. ";
 
-    @Override
-    public String toString() {
-        return dateFormat.format(value);
+    public String format(Locale locale) {
+        switch (locale.getLanguage()) {
+            case "cs":
+                return new SimpleDateFormat(CZECH_DAY_FORMAT).format(value) + czechDayOfTheWeek(value);
+            case "en":
+            default:
+                return new SimpleDateFormat(ENGLISH_FORMAT).format(value);
+        }
+    }
+
+    private String czechDayOfTheWeek(java.util.Date value) {
+        String englishDayOfTheWeek = new SimpleDateFormat(ENGLISH_DAY_IN_WEEK_FORMAT).format(value);
+        switch (englishDayOfTheWeek) {
+            case "Mon": return "Po";
+            case "Tue": return "Út";
+            case "Wed": return "St";
+            case "Thu": return "Čt";
+            case "Fri": return "Pá";
+            case "Sat": return "So";
+            case "Sun": return "Ne";
+            default:
+                log.error("Cannot format date {}. Unknown day of the week [{}]", this, englishDayOfTheWeek);
+                return englishDayOfTheWeek;
+        }
     }
 }
---
+++ src/test/java/com/github/jakubtomekcz/doctorscheduler/model/DateTest.java
@@ -0,0 +1,47 @@
+package com.github.jakubtomekcz.doctorscheduler.model;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import static com.github.jakubtomekcz.doctorscheduler.constant.PersonAndDateTestConstants.date;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class DateTest {
+
+    private static final Locale CZECH = new Locale("cs");
+
+    @ParameterizedTest
+    @MethodSource("formatMethodSource")
+    void format(Date date, Locale locale, String expectedResult) {
+        String actualResult = date.format(locale);
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> formatMethodSource() {
+        return Stream.of(
+                Arguments.of(date("2021-12-01"), Locale.ENGLISH, "Wed 01 Dec 2021"),
+                Arguments.of(date("2021-12-02"), Locale.ENGLISH, "Thu 02 Dec 2021"),
+                Arguments.of(date("2021-12-03"), Locale.ENGLISH, "Fri 03 Dec 2021"),
+                Arguments.of(date("2021-12-04"), Locale.ENGLISH, "Sat 04 Dec 2021"),
+                Arguments.of(date("2021-12-05"), Locale.ENGLISH, "Sun 05 Dec 2021"),
+                Arguments.of(date("2021-12-06"), Locale.ENGLISH, "Mon 06 Dec 2021"),
+                Arguments.of(date("2021-12-07"), Locale.ENGLISH, "Tue 07 Dec 2021"),
+                Arguments.of(date("2021-12-08"), Locale.ENGLISH, "Wed 08 Dec 2021"),
+
+                Arguments.of(date("2021-12-01"), CZECH, "1. St"),
+                Arguments.of(date("2021-12-02"), CZECH, "2. Čt"),
+                Arguments.of(date("2021-12-03"), CZECH, "3. Pá"),
+                Arguments.of(date("2021-12-04"), CZECH, "4. So"),
+                Arguments.of(date("2021-12-05"), CZECH, "5. Ne"),
+                Arguments.of(date("2021-12-06"), CZECH, "6. Po"),
+                Arguments.of(date("2021-12-07"), CZECH, "7. Út"),
+                Arguments.of(date("2021-12-08"), CZECH, "8. St"),
+
+                Arguments.of(date("2021-12-01"), Locale.CHINESE, "Wed 01 Dec 2021"));
+    }
+}
