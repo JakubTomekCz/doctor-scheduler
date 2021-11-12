51bf8e61f665faf785d455e32028bf35cb0e4f5f
JakubTomekCz <jakubtomek@jakubtomek.cz>
2021-11-12 21:25:47 +0100

    using SessionLocaleResolver

--- src/main/java/com/github/jakubtomekcz/doctorscheduler/i18/LocaleConfiguration.java
+++ src/main/java/com/github/jakubtomekcz/doctorscheduler/i18/LocaleConfiguration.java
@@ -3,21 +3,28 @@ package com.github.jakubtomekcz.doctorscheduler.i18;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-
-import java.util.List;
-import java.util.Locale;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
-        localeResolver.setDefaultLocale(Locale.ENGLISH);
-        localeResolver.setSupportedLocales(List.of(Locale.US, Locale.ENGLISH, new Locale("cs", "cz"),
-                new Locale("cs")));
-        return localeResolver;
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
