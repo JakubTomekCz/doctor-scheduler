c273bd1eb98c6f11d6a675b93befc03a852cf551
JakubTomekCz <jakubtomek@jakubtomek.cz>
2022-01-22 13:13:54 +0100

    added more simple tests into ScheduleSummaryTest

--- src/test/java/com/github/jakubtomekcz/doctorscheduler/model/ScheduleSummaryTest.java
+++ src/test/java/com/github/jakubtomekcz/doctorscheduler/model/ScheduleSummaryTest.java
@@ -35,6 +35,52 @@ class ScheduleSummaryTest {
                         HOMER, 1));
     }
 
+    @Test
+    void getWeekendDayTotals() {
+        PreferenceTable preferenceTable = mockPreferenceTable();
+        Schedule schedule = ScheduleBuilder.forPreferenceTable(preferenceTable)
+                .put(dDayPlusNDays(1), LENNY)
+                .put(dDayPlusNDays(2), CARL)
+                .put(dDayPlusNDays(3), HOMER)
+                .put(dDayPlusNDays(4), LENNY)
+                .put(dDayPlusNDays(5), CARL)
+                .put(dDayPlusNDays(6), HOMER)
+                .put(dDayPlusNDays(7), LENNY)
+                .put(dDayPlusNDays(8), CARL)
+                .build();
+
+        ScheduleSummary scheduleSummary = ScheduleSummary.of(preferenceTable, schedule);
+
+        assertThat(scheduleSummary.getWeekendDayTotals())
+                .isEqualTo(ImmutableMap.of(
+                        LENNY, 0,
+                        CARL, 1,
+                        HOMER, 1));
+    }
+
+    @Test
+    void getAllDayTotals() {
+        PreferenceTable preferenceTable = mockPreferenceTable();
+        Schedule schedule = ScheduleBuilder.forPreferenceTable(preferenceTable)
+                .put(dDayPlusNDays(1), LENNY)
+                .put(dDayPlusNDays(2), CARL)
+                .put(dDayPlusNDays(3), HOMER)
+                .put(dDayPlusNDays(4), LENNY)
+                .put(dDayPlusNDays(5), CARL)
+                .put(dDayPlusNDays(6), HOMER)
+                .put(dDayPlusNDays(7), LENNY)
+                .put(dDayPlusNDays(8), CARL)
+                .build();
+
+        ScheduleSummary scheduleSummary = ScheduleSummary.of(preferenceTable, schedule);
+
+        assertThat(scheduleSummary.getAllDayTotals())
+                .isEqualTo(ImmutableMap.of(
+                        LENNY, 3,
+                        CARL, 3,
+                        HOMER, 2));
+    }
+
     private PreferenceTable mockPreferenceTable() {
         PreferenceTable.Builder builder = PreferenceTable.builder();
         for (int i = 1; i <= 8; i++) {
